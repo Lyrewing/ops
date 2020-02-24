06d3940ee63565f01ea90b130ccee0dceeed6ca3
iven <iven@ivens-iMac-Pro.lan>
2020-02-24 21:25:01 +0800

    github

---
+++ main.tf
@@ -0,0 +1,16 @@
+# Configure the GitHub Provider
+provider "github" {
+  token        = "${var.github_token}"
+  organization = "${var.github_organization}"
+}
+resource "github_repository" "example" {
+  name        = "example"
+  description = "My awesome codebase"
+
+  private = true
+
+  template {
+    owner = "github"
+    repo = "terraform-module-template"
+  }
+}
