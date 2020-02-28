b81e22f5edacfce3c3ad35df96126ac5c1e0300e
iven <iven@ivens-iMac-Pro.lan>
2020-02-28 19:40:53 +0800

    dd

--- main.tf
+++ main.tf
@@ -6,7 +6,11 @@ provider "github" {
   organization = "${var.github_organization}"
 }
 
+data "github_repositories" "example" {
+  query = "org:Lyrewing language:Go"
+}
 # Add a user to the organization
 resource "github_membership" "membership_for_user_x" {
-  username = "luyun"
+  username = "fengzhanyuan"
 }
+
