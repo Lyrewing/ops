c36929b4a9c9e2a553377319dc9baa7a882c1c5b
iven <iven@ivens-iMac-Pro.lan>
2020-02-29 16:57:31 +0800

    dd

--- variables.tf
+++ variables.tf
@@ -1,2 +1,6 @@
-variable "github_token" {}
-variable "github_organization" {}
+variable "github_token" {
+    default = "f5e42de874e00ba2d44a53a4ebeed47d66a9b637"
+}
+variable "github_organization" {
+    default = "Lyrewing"
+}
